8fc77a6ee190b3929de670a75670c25827ca4e3a
BBLv <hyn02232@dev-pop.nri.co.jp>
2023-08-10 14:36:52 +0800

    Form-2

---
+++ FFModifier/Forms/FFFormShowAndHidden.swift
@@ -0,0 +1,18 @@
+//
+//  FFFormShowAndHidden.swift
+//  FFModifier
+//
+//  Created by BBLv on 2023/8/10.
+//
+
+import SwiftUI
+
+struct FFFormShowAndHidden: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FFFormShowAndHidden()
+}
